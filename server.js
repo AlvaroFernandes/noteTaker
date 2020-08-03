ca044c53f46296b1825af8b3bf0b0bb5ebde71bc
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-08-03 16:19:34 +1000

    update server.js with read the db.json file

--- server.js
+++ server.js
@@ -2,12 +2,21 @@
 
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//read db.JSON file
+app.get("./api/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utgf8", (err, result) => {
+        if (err) throw err;
+        return res.json(JSON.parse(result));
+    })
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
